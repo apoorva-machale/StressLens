d3cc48becebf787e31dac7ece194664a6de9235e
csuftitan <csuftitan@LTMacPro13-intel-164311.local>
2024-02-22 15:21:45 -0800

    login and authentication

--- blog/main.py
+++ blog/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from . import models
 from .database import engine
-from .routers import blog, user
+from .routers import blog, user, authentication
 
 #any new model found create on db
 models.Base.metadata.create_all(engine)
@@ -10,7 +10,7 @@ app = FastAPI()
 
 app.include_router(blog.router)
 app.include_router(user.router)
-
+app.include_router(authentication.router)
 
 
 
--- blog/schemas.py
+++ blog/schemas.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-from typing import List
+from typing import List, Optional
 
 class BlogBase(BaseModel):
     title: str
@@ -27,6 +27,19 @@ class User(BaseModel):
     name: str
     email: str
     password: str
+
+class Login(BaseModel):
+    username: str
+    password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    email: Optional[str] = None
+
 
 
 
