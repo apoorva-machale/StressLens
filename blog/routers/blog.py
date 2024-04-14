1dc1eaa03937ce82b7f5d6411c1fce78ca1d63c3
Apoorva <apoorvaa.machale@gmail.com>
2024-04-14 12:20:36 -0700

    code with sentiment analysis running removing authentication

--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -12,21 +12,21 @@ router = APIRouter(
 get_db = database.get_db
 
 @router.post('/', status_code=status.HTTP_201_CREATED)
-def analyze_blog(request: schemas.Blog, db: Session = Depends(get_db),current_user: schemas.User = Depends(oauth2.get_current_user)):
+def analyze_blog(request: schemas.Blog, db: Session = Depends(get_db)):
     return blog.analyze_blog(request, db)
 
 @router.get('/', response_model=List[schemas.ShowBlog])
-def all(db: Session = Depends(database.get_db), current_user: schemas.User = Depends(oauth2.get_current_user)):                                                                 
+def all(db: Session = Depends(database.get_db)):                                                                 
     return blog.get_all(db)
 
 @router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
-def destroy(id: int, db: Session = Depends(get_db), current_user: schemas.User = Depends(oauth2.get_current_user)):
+def destroy(id: int, db: Session = Depends(get_db)):
     return blog.destroy(id,db)
 
 @router.put('/{id}', status_code=status.HTTP_202_ACCEPTED)
-def update(id: int, request: schemas.Blog, db: Session = Depends(get_db), current_user: schemas.User = Depends(oauth2.get_current_user)):
+def update(id: int, request: schemas.Blog, db: Session = Depends(get_db)):
    return blog.update(id, db)
 
 @router.get('/{id}',status_code=200, response_model=schemas.ShowBlog)
-def show(id: int, db:Session = Depends(get_db),current_user: schemas.User = Depends(oauth2.get_current_user)):
+def show(id: int, db:Session = Depends(get_db)):
     return blog.show(id, db)
