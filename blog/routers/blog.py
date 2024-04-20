3a6d1072ccc0ca885cabf5d92c7a9bc1b2ce0eec
shrutimath99 <shrutimath99@gmail.com>
2024-04-20 23:28:12 +0530

    Authorizartion tokrn and verify token function added

--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -1,44 +1,65 @@
 from typing import List, Union
-from fastapi import APIRouter, HTTPException, Depends, status
+from fastapi import APIRouter, HTTPException, Depends, status,Header,Request
 from .. import schemas, database, oauth2
 from sqlalchemy.orm import Session
 from ..repository import blog
+from ..token import verify_token
 
 
 router = APIRouter(
     prefix="/blog",
-    tags=['Blogs']
+    tags=['Blogs'],
+    
+    
 )
 get_db = database.get_db
 
 @router.post('/', status_code=status.HTTP_201_CREATED)
-async def analyze_blog(request: schemas.BlogBase, db: Session = Depends(database.get_db)):
-    return await blog.analyze_blog(request, db)
+async def analyze_blog(request_body: schemas.BlogBase, test_h:str= Header() ,db: Session = Depends(database.get_db)):
+    print("This is Headerrrr",test_h)
+   
+    return await blog.analyze_blog(request_body, db)
 
 @router.get('/', response_model=List[schemas.Blog])
-def get_all(email:str, db: Session = Depends(database.get_db)):                                                                 
+def get_all(db: Session = Depends(database.get_db), test_h:str= Header()): 
+    print("This is Headerrrr Autho",test_h)
+    email = verify_token(test_h)                                 
     return blog.get_all(email, db)
 
 @router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
-def destroy(id: int, db: Session = Depends(database.get_db)):
+def destroy(id: int, db: Session = Depends(database.get_db),test_h:str= Header()):
+    print("This is Headerrrr Autho",test_h)
+    email = verify_token(test_h)
     return blog.destroy(id,db)
 
 @router.get('/output', response_model=List[schemas.ShowBlog])
-def get_blogs_for_date(date: str, email:str, db: Session = Depends(database.get_db)):                                                                 
+def get_blogs_for_date(date: str,db: Session = Depends(database.get_db),test_h:str= Header()):                                                                 
+    
+    print("This is Headerrrr Autho",test_h)
+    email=verify_token(test_h)
     return blog.get_blogs_for_date(date, email, db)
 
 @router.get('/output_for_date_range', response_model=List[schemas.ShowBlog])
-def get_blogs_for_date_range(from_date: str, to_date: str, email:str, db: Session = Depends(database.get_db)):                                                                 
+def get_blogs_for_date_range(from_date: str, to_date: str,
+                              db: Session = Depends(database.get_db), test_h:str= Header()):                                                                 
+    print("This is Headerrrr Autho",test_h)
+    email=verify_token(test_h)
     return blog.get_blogs_for_date_range(from_date, to_date, email, db)
 
 @router.get('/classify_blog', response_model=List[schemas.ShowCategory])
-async def classify_blog(date: str, email:str, db: Session = Depends(database.get_db)):
+async def classify_blog(date: str, db: Session = Depends(database.get_db),test_h:str= Header()):
+    print("This is Headerrrr Autho",test_h)
+    email=verify_token(test_h)
     return await blog.classify_blog(date, email, db)
 
 @router.get('/classify_blog_id', response_model=List[schemas.ShowCategory])
-async def classify_blog_id(blog_id:int, db: Session = Depends(database.get_db)):
+async def classify_blog_id(blog_id:int, db: Session = Depends(database.get_db),test_h:str= Header()):
+    print("This is Headerrrr Autho",test_h)
+
     return await blog.classify_blog_id(blog_id, db)
 
 @router.get('/{email}',status_code=200, response_model=schemas.ShowBlog)
-def show(email: str, db:Session = Depends(get_db)):
+def show(test_h:str= Header(), db:Session = Depends(get_db)):
+    print("This is Headerrrr Autho",test_h)
+    email=verify_token(test_h)
     return blog.show(email, db)
--- blog/token.py
+++ blog/token.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timedelta
 from jose import JWTError, jwt
 from . import schemas
+from fastapi import  HTTPException,status
 
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
@@ -13,13 +14,15 @@ def create_access_token(data: dict):
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
-def verify_token(token:str,credentials_exception):
+def verify_token(token:str):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         email: str = payload.get("sub")
         if email is None:
-            raise credentials_exception
+            raise HTTPException(status_code= status.HTTP_401_UNAUTHORIZED)
         token_data = schemas.TokenData(email=email)
     except JWTError:
-        raise credentials_exception
+        raise HTTPException(status_code= status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+    except Exception as e:
+        raise HTTPException(status_code= status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
     
